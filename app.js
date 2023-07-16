566927253f86f4860916fe5a4eedf01e5445c2d1
Angathan003 <angathan003@gmail.com>
2023-07-16 21:28:47 +0530

    add background

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-window.addEventListener('scroll', function() {
+function updateBackgroundPosition() {
     var scrollPosition = window.pageYOffset;
     var landingSection = document.querySelector('.landing');
     landingSection.style.backgroundPosition = 'center ' + (-scrollPosition * 0.5) + 'px';
-});
+    requestAnimationFrame(updateBackgroundPosition);
+  }
+  
+  window.addEventListener('scroll', function() {
+    requestAnimationFrame(updateBackgroundPosition);
+  });
+  
